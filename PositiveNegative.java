960c2db99779e4f43dcba254bd7ca169f214c666
divyasreebollu <141166868+divyasreebollu@users.noreply.github.com>
2023-10-01 22:41:22 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,28 @@
+class PositiveNegative
+{
+public static void main(String[] args)
+{
+int n=2;
+if(n>0)
+{
+System.out.println("Positive");
+}
+else if(n<0)
+{
+System.out.println("Negative");
+}
+else{
+System.out.println("Zero");
+}
+}
+}
+
+execution results:
+C:\Users\B Divya sreee>D:
+
+D:\>cd sree
+
+D:\sree>javac PositiveNegative.java
+
+D:\sree>java PositiveNegative
+Positive
